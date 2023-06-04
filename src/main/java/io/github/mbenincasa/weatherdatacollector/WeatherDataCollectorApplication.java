15e95b29ee28da2c827b327fb5db5522b4ab3941
MBenincasa <mirkobenincasa44@gmail.com>
2023-06-04 17:41:27 +0200

    Initial commit

---
+++ src/main/java/io/github/mbenincasa/weatherdatacollector/WeatherDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package io.github.mbenincasa.weatherdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mbenincasa/weatherdatacollector/WeatherDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mbenincasa.weatherdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
